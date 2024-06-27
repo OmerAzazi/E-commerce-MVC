77ac5d44d64038763872502c0b7a7d2f6818fce5
OmerAzazi <98057647+OmerAzazi@users.noreply.github.com>
2024-06-27 12:38:59 +0300

    Create Category Model

---
+++ E-commerce/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace E_commerce.Models
+{
+    public class Category
+    {
+        //prop
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
