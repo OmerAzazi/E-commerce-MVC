0f3a386651b4f62bd5d432a9c4f3aac5785761d9
OmerAzazi <98057647+OmerAzazi@users.noreply.github.com>
2024-06-28 00:48:16 +0300

    Get all Categories

--- E-commerce/Controllers/CategoryController.cs
+++ E-commerce/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_commerce.Data;
+using E_commerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_commerce.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+            // We already have the method in program.cs so we just call it 
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
