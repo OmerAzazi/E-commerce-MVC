1183089091b6b9fd7058ea07f6091ca1f61ef8e8
OmerAzazi <98057647+OmerAzazi@users.noreply.github.com>
2024-06-28 02:25:34 +0300

    Create Category

--- E-commerce/Controllers/CategoryController.cs
+++ E-commerce/Controllers/CategoryController.cs
@@ -8,14 +8,28 @@ namespace E_commerce.Controllers
     {
         private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db)
-            // We already have the method in program.cs so we just call it 
+        // We already have the method in program.cs so we just call it 
         {
             _db = db;
         }
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost] //Bz of the form
+        public IActionResult Create(Category obj)
+            // obj is the data that we get form the website 
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
