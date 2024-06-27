5f566b9b77cef9fa8ff5516dc30c2c957242000d
OmerAzazi <98057647+OmerAzazi@users.noreply.github.com>
2024-06-27 12:46:47 +0300

    Data Annotations

--- E-commerce/Models/Category.cs
+++ E-commerce/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace E_commerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce.Models
 {
     public class Category
     {
         //prop
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
 
